abe14b60174b9e0a9d1f7b8a9e500ec0dc126946
yue0707 <cullen.guimond@gmail.com>
2025-05-13 14:25:03 -0400

    Built Fluent Nexus for .NET 6.0 and exported release DLL to ORMIs directory under imports

--- import/FluentNexus/Models/ModFileWithHash.cs
+++
@@ -1,9 +0,0 @@
-namespace Pathoschild.FluentNexus.Models
-{
-    /// <summary>A downloadable mod file, with its MD5 hash.</summary>
-    public class ModFileWithHash : ModFile
-    {
-        /// <summary>The MD5 file hash.</summary>
-        public string Md5 { get; set; }
-    }
-}
